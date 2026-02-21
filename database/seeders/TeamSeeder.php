f014b397b2545329dcc06eac1a4a3b10383a42da
NachoFernan <nachofernan@gmail.com>
2026-02-21 16:48:54 -0300

    actualizado

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -11,76 +11,76 @@ class TeamSeeder extends Seeder
     {
         $teams = [
             // Grupo A
-            ['name' => 'México',            'fifa_code' => 'MEX'],
-            ['name' => 'Sudáfrica',         'fifa_code' => 'RSA'],
-            ['name' => 'Corea del Sur',     'fifa_code' => 'KOR'],
-            ['name' => 'Repechaje UEFA D',  'fifa_code' => 'PO4'], // Dinamarca/Chequia/Macedonia/Irlanda
+            ['name' => 'México',            'fifa_code' => 'MEX',  'flag_url' => 'mx'],
+            ['name' => 'Sudáfrica',         'fifa_code' => 'RSA',  'flag_url' => 'za'],
+            ['name' => 'Corea del Sur',     'fifa_code' => 'KOR',  'flag_url' => 'kr'],
+            ['name' => 'Repechaje UEFA D',  'fifa_code' => 'PO4',  'flag_url' => null], // Dinamarca/Chequia/Macedonia/Irlanda
 
             // Grupo B
-            ['name' => 'Canadá',            'fifa_code' => 'CAN'],
-            ['name' => 'Repechaje UEFA A',  'fifa_code' => 'PO1'], // Italia/Irlanda del Norte/Gales/Bosnia
-            ['name' => 'Qatar',             'fifa_code' => 'QAT'],
-            ['name' => 'Suiza',             'fifa_code' => 'SUI'],
+            ['name' => 'Canadá',            'fifa_code' => 'CAN',  'flag_url' => 'ca'],
+            ['name' => 'Repechaje UEFA A',  'fifa_code' => 'PO1',  'flag_url' => null], // Italia/Irlanda del Norte/Gales/Bosnia
+            ['name' => 'Qatar',             'fifa_code' => 'QAT',  'flag_url' => 'qa'],
+            ['name' => 'Suiza',             'fifa_code' => 'SUI',  'flag_url' => 'ch'],
 
             // Grupo C
-            ['name' => 'Brasil',            'fifa_code' => 'BRA'],
-            ['name' => 'Marruecos',         'fifa_code' => 'MAR'],
-            ['name' => 'Escocia',           'fifa_code' => 'SCO'],
-            ['name' => 'Haití',             'fifa_code' => 'HAI'],
+            ['name' => 'Brasil',            'fifa_code' => 'BRA',  'flag_url' => 'br'],
+            ['name' => 'Marruecos',         'fifa_code' => 'MAR',  'flag_url' => 'ma'],
+            ['name' => 'Escocia',           'fifa_code' => 'SCO',  'flag_url' => 'gb-sct'],
+            ['name' => 'Haití',             'fifa_code' => 'HAI',  'flag_url' => 'ht'],
 
             // Grupo D
-            ['name' => 'Estados Unidos',    'fifa_code' => 'USA'],
-            ['name' => 'Paraguay',          'fifa_code' => 'PAR'],
-            ['name' => 'Australia',         'fifa_code' => 'AUS'],
-            ['name' => 'Repechaje UEFA C',  'fifa_code' => 'PO3'], // Turquía/Rumanía/Eslovaquia/Kosovo
+            ['name' => 'Estados Unidos',    'fifa_code' => 'USA',  'flag_url' => 'us'],
+            ['name' => 'Paraguay',          'fifa_code' => 'PAR',  'flag_url' => 'py'],
+            ['name' => 'Australia',         'fifa_code' => 'AUS',  'flag_url' => 'au'],
+            ['name' => 'Repechaje UEFA C',  'fifa_code' => 'PO3',  'flag_url' => null], // Turquía/Rumanía/Eslovaquia/Kosovo
 
             // Grupo E
-            ['name' => 'Alemania',          'fifa_code' => 'GER'],
-            ['name' => 'Curazao',           'fifa_code' => 'CUW'],
-            ['name' => 'Costa de Marfil',   'fifa_code' => 'CIV'],
-            ['name' => 'Ecuador',           'fifa_code' => 'ECU'],
+            ['name' => 'Alemania',          'fifa_code' => 'GER',  'flag_url' => 'de'],
+            ['name' => 'Curazao',           'fifa_code' => 'CUW',  'flag_url' => 'cw'],
+            ['name' => 'Costa de Marfil',   'fifa_code' => 'CIV',  'flag_url' => 'ci'],
+            ['name' => 'Ecuador',           'fifa_code' => 'ECU',  'flag_url' => 'ec'],
 
             // Grupo F
-            ['name' => 'Países Bajos',      'fifa_code' => 'NED'],
-            ['name' => 'Japón',             'fifa_code' => 'JPN'],
-            ['name' => 'Repechaje UEFA B',  'fifa_code' => 'PO2'], // Ucrania/Suecia/Polonia/Albania
-            ['name' => 'Túnez',             'fifa_code' => 'TUN'],
+            ['name' => 'Países Bajos',      'fifa_code' => 'NED',  'flag_url' => 'nl'],
+            ['name' => 'Japón',             'fifa_code' => 'JPN',  'flag_url' => 'jp'],
+            ['name' => 'Repechaje UEFA B',  'fifa_code' => 'PO2',  'flag_url' => null], // Ucrania/Suecia/Polonia/Albania
+            ['name' => 'Túnez',             'fifa_code' => 'TUN',  'flag_url' => 'tn'],
 
             // Grupo G
-            ['name' => 'Bélgica',           'fifa_code' => 'BEL'],
-            ['name' => 'Egipto',            'fifa_code' => 'EGY'],
-            ['name' => 'Irán',              'fifa_code' => 'IRN'],
-            ['name' => 'Nueva Zelanda',     'fifa_code' => 'NZL'],
+            ['name' => 'Bélgica',           'fifa_code' => 'BEL',  'flag_url' => 'be'],
+            ['name' => 'Egipto',            'fifa_code' => 'EGY',  'flag_url' => 'eg'],
+            ['name' => 'Irán',              'fifa_code' => 'IRN',  'flag_url' => 'ir'],
+            ['name' => 'Nueva Zelanda',     'fifa_code' => 'NZL',  'flag_url' => 'nz'],
 
             // Grupo H
-            ['name' => 'España',            'fifa_code' => 'ESP'],
-            ['name' => 'Cabo Verde',        'fifa_code' => 'CPV'],
-            ['name' => 'Arabia Saudita',    'fifa_code' => 'KSA'],
-            ['name' => 'Uruguay',           'fifa_code' => 'URU'],
+            ['name' => 'España',            'fifa_code' => 'ESP',  'flag_url' => 'es'],
+            ['name' => 'Cabo Verde',        'fifa_code' => 'CPV',  'flag_url' => 'cv'],
+            ['name' => 'Arabia Saudita',    'fifa_code' => 'KSA',  'flag_url' => 'sa'],
+            ['name' => 'Uruguay',           'fifa_code' => 'URU',  'flag_url' => 'uy'],
 
             // Grupo I
-            ['name' => 'Francia',           'fifa_code' => 'FRA'],
-            ['name' => 'Noruega',           'fifa_code' => 'NOR'],
-            ['name' => 'Senegal',           'fifa_code' => 'SEN'],
-            ['name' => 'Repechaje FIFA 2',  'fifa_code' => 'RF2'], // Bolivia/Surinam/Irak
+            ['name' => 'Francia',           'fifa_code' => 'FRA',  'flag_url' => 'fr'],
+            ['name' => 'Noruega',           'fifa_code' => 'NOR',  'flag_url' => 'no'],
+            ['name' => 'Senegal',           'fifa_code' => 'SEN',  'flag_url' => 'sn'],
+            ['name' => 'Repechaje FIFA 2',  'fifa_code' => 'RF2',  'flag_url' => null], // Bolivia/Surinam/Irak
 
             // Grupo J
-            ['name' => 'Argentina',         'fifa_code' => 'ARG'],
-            ['name' => 'Argelia',           'fifa_code' => 'ALG'],
-            ['name' => 'Austria',           'fifa_code' => 'AUT'],
-            ['name' => 'Jordania',          'fifa_code' => 'JOR'],
+            ['name' => 'Argentina',         'fifa_code' => 'ARG',  'flag_url' => 'ar'],
+            ['name' => 'Argelia',           'fifa_code' => 'ALG',  'flag_url' => 'dz'],
+            ['name' => 'Austria',           'fifa_code' => 'AUT',  'flag_url' => 'at'],
+            ['name' => 'Jordania',          'fifa_code' => 'JOR',  'flag_url' => 'jo'],
 
             // Grupo K
-            ['name' => 'Portugal',          'fifa_code' => 'POR'],
-            ['name' => 'Repechaje FIFA 1',  'fifa_code' => 'RF1'], // RD Congo/Jamaica/Nueva Caledonia
-            ['name' => 'Uzbekistán',        'fifa_code' => 'UZB'],
-            ['name' => 'Colombia',          'fifa_code' => 'COL'],
+            ['name' => 'Portugal',          'fifa_code' => 'POR',  'flag_url' => 'pt'],
+            ['name' => 'Repechaje FIFA 1',  'fifa_code' => 'RF1',  'flag_url' => null], // RD Congo/Jamaica/Nueva Caledonia
+            ['name' => 'Uzbekistán',        'fifa_code' => 'UZB',  'flag_url' => 'uz'],
+            ['name' => 'Colombia',          'fifa_code' => 'COL',  'flag_url' => 'co'],
 
             // Grupo L
-            ['name' => 'Inglaterra',        'fifa_code' => 'ENG'],
-            ['name' => 'Croacia',           'fifa_code' => 'CRO'],
-            ['name' => 'Ghana',             'fifa_code' => 'GHA'],
-            ['name' => 'Panamá',            'fifa_code' => 'PAN'],
+            ['name' => 'Inglaterra',        'fifa_code' => 'ENG',  'flag_url' => 'gb-eng'],
+            ['name' => 'Croacia',           'fifa_code' => 'CRO',  'flag_url' => 'hr'],
+            ['name' => 'Ghana',             'fifa_code' => 'GHA',  'flag_url' => 'gh'],
+            ['name' => 'Panamá',            'fifa_code' => 'PAN',  'flag_url' => 'pa'],
         ];
 
         foreach ($teams as $team) {
